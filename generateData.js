47a3cf6f7c11c635cee8ee2602d494c78cfd9c3c
Brejndead <91755421+wernisch@users.noreply.github.com>
2025-11-15 01:00:30 +0100

    Update generateData.js

--- generateData.js
+++ generateData.js
@@ -1,18 +1,151 @@
 import fs from "fs";
 import fetch from "node-fetch";
 
-const gameIds = [
-  8649112027, 8641794993, 8617745696, 8470216398, 8120277194, 8030001602,
-  6431757712, 7016268111, 6789766645, 6614175388, 6528524000, 6463581673,
-  7096838238, 7166097502, 7517851351, 7626153268, 7072328729, 6743843913,
-  7334543566, 6829990681, 7263505269, 7401898945, 7309264740, 7456466538,
-  3071634329, 4800580998, 7288212525, 2505069317, 5049176019, 2946951335,
-  7424382390, 7168683817, 7349366409, 8154106881, 7923536197, 8091666772,
-  8631229462, 8385096583, 8294794009, 8034900041, 8271046459, 8886304312,
-  8418842748,
-  8908046810,
-  8914680488
-];
+const gameMeta = {
+  8649112027: { // Break Bones Tower
+    type: "My Game",
+    description: "Simple tower game where you jump off to break bones"
+  },
+  8641794993: { // Kids vs Parents Tower
+    type: "My Game",
+    description: "Simple tower game with two teams, kids and parents"
+  },
+  8617745696: { // Draw Troll Tower
+    type: "My Game",
+    description: "Simple tower game where you can draw on the walls"
+  },
+  8470216398: { // [🎃] Protect The House From Brainrots
+    type: "My Game",
+    description: "Game where brainrots try to attack the house and you must shoot them"
+  },
+  8120277194: { // [UPD]Squid Game Roleplay Tower
+    type: "Shareholder",
+    description: "Tower game with the theme of Squid Game"
+  },
+  6431757712: { // Find the Ducks 🐤
+    type: "My Game",
+    description: "Game where you find ducks"
+  },
+  7016268111: { // Find the Ducks 2 🐤
+    type: "My Game",
+    description: "Game where you find more ducks"
+  },
+  6789766645: { // Find the Doges 🐶
+    type: "My Game",
+    description: "Game where you find doges"
+  },
+  6614175388: { // Find the Monkeys 🐵
+    type: "My Game",
+    description: "Game where you find monkeys"
+  },
+  6528524000: { // Find the Frogs 🐸
+    type: "My Game",
+    description: "Game where you find frogs"
+  },
+  6463581673: { // Find the Teddy Bears 🧸
+    type: "My Game",
+    description: "Game where you find teddy bears"
+  },
+  7166097502: { // [UPD] Parkour Rush
+    type: "My Game",
+    description: "Obby game with advanced parkour system"
+  },
+  7626153268: { // 🏁 Parkour Obby [W7]
+    type: "My Game",
+    description: "Obby game with multiple worlds and advanced parkour system"
+  },
+  7334543566: { // Grow Your Duck! 🐤
+    type: "Contribution",
+    description: "Game where you're a duck and you eat other ducks"
+  },
+  6829990681: { // Planets Merge Tycoon
+    type: "Contribution",
+    description: "Game where you drop and merge planets"
+  },
+  7263505269: { // Find the Dinos 🦕
+    type: "My Game",
+    description: "Game where you find dinos"
+  },
+  7401898945: { // Midnight Stalker [HORROR]
+    type: "Shareholder",
+    description: "Horror slop"
+  },
+  7309264740: { // Midnight Groceries [HORROR]
+    type: "Shareholder",
+    description: "Horror slop"
+  },
+  7456466538: { // Midnight Easter [HORROR]
+    type: "Shareholder",
+    description: "Horror slop"
+  },
+  3071634329: { // Emily [HORROR]
+    type: "Shareholder",
+    description: "Horror slop"
+  },
+  4800580998: { // Hell Battlegrounds
+    type: "Shareholder",
+    description: "Combat battleground game"
+  },
+  7288212525: { // Emoji Murder
+    type: "Shareholder",
+    description: "Game where you guess emojis"
+  },
+  2505069317: { // Gold Mining Tycoon
+    type: "My Game",
+    description: "Game where you mine gold and advance to new worlds"
+  },
+  5049176019: { // Slingshot Obby [2 Player Obby]
+    type: "Shareholder",
+    description: "2 player obby with slingshot mechanic"
+  },
+  2946951335: { // Squid Game [S3]
+    type: "My Game",
+    description: "one more game one more game one more game"
+  },
+  7168683817: { // [Update 4] Crazy Shooter 🔫
+    type: "Shareholder",
+    description: "Basic FPS game"
+  },
+  7349366409: { // Blobfish Evolution
+    type: "Shareholder",
+    description: ""
+  },
+  8091666772: { // Build a Capybara Army 🍊
+    type: "My Game",
+    description: "Game where you have your own capybara army"
+  },
+  8631229462: { // Find the Ducks 3 🐤
+    type: "My Game",
+    description: "Find even more ducks"
+  },
+  8385096583: { // Swimming Brainrots 🏊
+    type: "My Game",
+    description: "Game where you have brainrots swimming in your pools and generate money"
+  },
+  8294794009: { // 99 Nights in the Bunker🐻 [FURNISH THE BUNKER]
+    type: "Contribution",
+    description: "Game where you survive in the bunker"
+  },
+  8886304312: { // Angry Brainrots💥🏹
+    type: "Contribution",
+    description: "Angry Birds but with brainrots"
+  },
+  8418842748: { // Brainrot Bridge Battles!
+    type: "Contribution",
+    description: "Bridge battles with brainrots"
+  },
+  8908046810: { // My Burger Factory 🍔 [UPD]
+    type: "Contribution",
+    description: "Game where you make burgers"
+  },
+  8914680488: { // Run or Die
+    type: "Contribution",
+    description: "Classic death run game"
+  }
+};
+
+
+const gameIds = Object.keys(gameMeta).map(Number);
 
 const proxyUrl = "https://workers-playground-white-credit-775c.bloxyhdd.workers.dev/?url=";
 const BATCH_SIZE = 75;
@@ -51,7 +184,11 @@ async function fetchWithRetry(url, init = {}) {
     try {
       const controller = new AbortController();
       const timer = setTimeout(() => controller.abort(), REQUEST_TIMEOUT_MS);
-      res = await fetch(url, { ...init, signal: controller.signal, headers: { ...(init.headers || {}), Origin: "null" } });
+      res = await fetch(url, {
+        ...init,
+        signal: controller.signal,
+        headers: { ...(init.headers || {}), Origin: "null" }
+      });
       clearTimeout(timer);
 
       if (res.status === 429 && attempt < MAX_ATTEMPTS) {
@@ -100,7 +237,9 @@ async function fetchVotesBatch(ids) {
 }
 
 async function fetchIconsBatch(ids) {
-  const url = wrap(`https://thumbnails.roblox.com/v1/games/multiget/thumbnails?universeIds=${ids.join(",")}&size=768x432&format=Png&isCircular=false`);
+  const url = wrap(
+    `https://thumbnails.roblox.com/v1/games/multiget/thumbnails?universeIds=${ids.join(",")}&size=768x432&format=Png&isCircular=false`
+  );
   const res = await fetchWithRetry(url);
   if (!res.ok) throw new Error(`thumbs ${res.status}`);
   const data = await res.json();
@@ -129,6 +268,8 @@ async function fetchIconsBatch(ids) {
         const game = gamesMap.get(id);
         if (!game) continue;
 
+        const meta = gameMeta[id] || {};
+
         allGames.push({
           id: game.id,
           rootPlaceId: game.rootPlaceId,
@@ -136,7 +277,9 @@ async function fetchIconsBatch(ids) {
           playing: game.playing || 0,
           visits: game.visits || 0,
           likeRatio: votesMap.get(id) ?? 0,
-          icon: iconsMap.get(id) ?? ""
+          icon: iconsMap.get(id) ?? "",
+          type: meta.type || "Contribution",
+          description: meta.description || ""
         });
       }
 
